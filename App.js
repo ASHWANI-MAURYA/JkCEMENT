1b905ccfd272d7a25143c686a7358e96a4891db8
Ashwani maurya <ashwanimaurya098@gmail.com>
2022-09-12 18:35:41 +0530

    stack and drawer is done on both

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './Navigation/StackNavigation'
-import DrawerNavigatior from './Navigation/DrawerNavigatior'
+// import DrawerNavigatior from './Navigation/DrawerNavigatior'
 const App = () => {
   return (
     <NavigationContainer>
       <StatusBar style="light" />
-      {/* <StackNavigation/> */}
-      <DrawerNavigatior/>
+      <StackNavigation/>
+      {/* <DrawerNavigatior/> */}
     </NavigationContainer>
   )
 }
